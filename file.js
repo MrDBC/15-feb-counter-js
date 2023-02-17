a2d3e4e03680418ed70253db882aacd4eb87d3f9
MrDBC <69349350+MrDBC@users.noreply.github.com>
2023-02-17 20:50:58 +0530

    first commit

---
+++ file.js
@@ -0,0 +1,13 @@
+const no= document.getElementById('number')
+
+const increment= ()=>{
+   
+    no.innerText++;
+}
+const decrement= ()=>{
+    
+    no.innerText--;
+}
+const reset= ()=>{
+    no.innerText =0;
+}
